5783d5587a19401d58574ed282d0a1627e998a5a
Nayra Lima <167091440+Ket1031@users.noreply.github.com>
2024-12-11 14:56:41 +0000

    updates

---
+++ Curso Programas/Java (orientada a objetos)/Aula 4/heranca/src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    public String tipo;
+    public String som;
+    int idade;
+
+    void fazSom(){
+        System.out.println("O" + tipo + som);
+    }
+}
+
+class Gato extends Animal {
+
+
+}
