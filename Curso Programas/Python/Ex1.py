4fe30f2d6adb24795fad51e477d84e892c599fc0
Nayra Lima <167091440+Ket1031@users.noreply.github.com>
2025-02-19 15:16:18 +0000

    updates de organizacao

--- Curso Programas/Python/Ex1.py
+++
@@ -1,7 +0,0 @@
-#exercicio 1 soma de dois numeros
-
-num1 = int(input("Digite um numero: "))
-num2 = int(input("Digite outro numero: "))
-soma = num1 + num2
-print(f"A soma de {num1} + {num2} é igual a {soma}")
-
--- Curso Programas/Python/Ex4.py
+++
@@ -1,3 +0,0 @@
-idade = int(input("Digite sua idade: "))
-dias = idade*365
-print(f"Você presenciou ao todo {dias} nasceres do sol.")
